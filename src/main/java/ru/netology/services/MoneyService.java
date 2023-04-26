77efd88e8ab8ab46efdc8702507104fb4bc03b7c
Stavinskaya_M <marina.st6277@gmail.com>
2023-04-26 19:13:27 +0600

    fixMonth

--- src/main/java/ru/netology/services/MoneyService.java
+++ src/main/java/ru/netology/services/MoneyService.java
@@ -4,18 +4,18 @@ public class MoneyService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; // кол-во месяцев отдыха
         int money = 0; //сумма на счету
-        int mouth = 0;
+        int month = 0;
 
         for (count = 1; count <= 12; count++) {
             if (threshold <= money) {
-                mouth++; // увеличиваем счётчик месяцев отдыха
+                month++; // увеличиваем счётчик месяцев отдыха
                 money = money - expenses;
                 money = money / 3;
             } else {
                 money = money + income - expenses;
             }
         }
-        return mouth;
+        return month;
 
     }
 }
