bd4be29c7a39afca0ee27c2c022f9acc2d6c9679
Stavinskaya_M <marina.st6277@gmail.com>
2023-04-26 18:46:54 +0600

    TestCsv

--- src/main/java/ru/netology/services/MoneyService.java
+++ src/main/java/ru/netology/services/MoneyService.java
@@ -1,17 +1,13 @@
 package ru.netology.services;
 
 public class MoneyService {
-    int income; // доход за месяц
-    int expenses; // обязательные траты
-    int threshold; // если на счету есть сумма, то выбираем отдых, иначе — работу.
-
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; // кол-во месяцев отдыха
         int money = 0; //сумма на счету
         int mouth = 0;
 
         for (count = 1; count <= 12; count++) {
-            if (threshold <= money) { // можем ли отдыхать?
+            if (threshold <= money) {
                 mouth++; // увеличиваем счётчик месяцев отдыха
                 money = money - expenses;
                 money = money / 3;
--- src/test/java/ru/netology/services/MoneyServiceTest.java
+++ src/test/java/ru/netology/services/MoneyServiceTest.java
@@ -1,25 +1,16 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class MoneyServiceTest {
-    @Test
-    public void testWithSalaryMin(){
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/input.csv")
+    public void testWithSalaryMin(int expected, int income, int expenses, int threshold) {
         MoneyService service = new MoneyService();
-        int expected = 3; // ожидаемый
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-    @Test
-    public void testWithSalaryMax(){
-        MoneyService service = new MoneyService();
-        int expected = 2; // ожидаемый
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-
 }
